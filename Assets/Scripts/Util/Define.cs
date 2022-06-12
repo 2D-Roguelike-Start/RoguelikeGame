602c178f769444f6fae840834b232b0fe8138769
dasfee33 <yeongsanggim08@gmail.com>
2022-06-12 23:27:40 +0900

    현재진행상황
    현재진행상황

---
+++ Assets/Scripts/Util/Define.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Define
+{
+    public enum Scene
+    {
+        Unknown,
+        Tutorial,
+        Stage1,
+        Lobby,
+        Game,
+    }
+
+
+    public enum MouseEvent
+    {
+        Press,
+        Click,
+    }
+
+    public enum UIEvent
+    {
+        Click,
+        Drag,
+    }
+}
+
+
